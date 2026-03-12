3f0a4011690c2aa2e57e830a048aa40b9712504c
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-03-12 21:27:22 +0300

    Add files via upload

---
+++ 09_maltiple_declaration/09_maltiple_declaration.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var coffee, milk, sugar = 1, 2, 1
+	// var coffee = 1
+	// var milk = 2
+	// var sugar = 1
+
+	fmt.Println("Coffee:", coffee, "Milk:", milk, "Sugar:", sugar)
+}
